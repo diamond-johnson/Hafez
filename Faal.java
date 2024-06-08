eac8af746e35a11c071b055da8e1104edc01c300
diamond-johnson <diamond716729@gmail.com>
2024-06-08 18:06:20 +0330

    Create Faal.java

---
+++ Faal.java
@@ -0,0 +1,26 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Faal {
+    @JsonProperty("Poem")
+    private String Poem;
+
+    @JsonProperty("Interpretation")
+    private String Interpretation;
+
+
+    public String getPoem() {
+        return Poem;
+    }
+
+    public void setPoem(String poem) {
+        this.Poem = poem;
+    }
+
+    public String getInterpretation() {
+        return Interpretation;
+    }
+
+    public void setInterpretation(String interpretation) {
+        this.Interpretation = interpretation;
+    }
+}
